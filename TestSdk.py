d88d4adda4310cb9f458f51c42b1f45ab5fc7d47
chenxuanjin <13802885422@139.com>
2018-12-03 16:51:09 +0800

    初步完成2

--- TestSdk.py
+++ TestSdk.py
@@ -23,6 +23,7 @@ class MyTestCase(unittest.TestCase):
         desired_caps['appPackage'] = 'com.cmic.testerapp'
         desired_caps['appActivity'] = 'com.cmic.testerapp.MainActivity'
         desired_caps['noReset'] = 'true'
+        desired_caps['unicodeKeyboard'] = True
         desired_caps['app'] = os.path.abspath('..') + '/app-release.apk'
 
         driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
@@ -33,7 +34,9 @@ class MyTestCase(unittest.TestCase):
 
     def test_01(self):
         try:
-            self.driver.find_element_by_xpath("//*[@text='允许']").click()
+            location = ("xpath", "//*[@text='允许']")
+            el = WebDriverWait(self.driver, 1, 0.5).until(EC.presence_of_element_located(location))
+            el.click()
         except:
             pass
         try:
@@ -53,17 +56,17 @@ class MyTestCase(unittest.TestCase):
             except:
                 pass
         el = self.driver.find_element_by_id("com.cmic.testerapp:id/et_package_name")
-        # self.driver.set_value(el,"com.example.app.debug")这样写的话.会变成。
-        el.send_keys("com.example.app")
+        self.driver.set_value(el, "com.example.app.debug")
         time.sleep(1)
         self.driver.find_element_by_android_uiautomator("text(\"导入配置文件\")").click()
         # 获取不到Toast
-        # toast_loc = ("xpath", ".//*[contains(@text,'导入配置文件成功')]")
-        # e = WebDriverWait(self.driver, 5, 0.01).until(EC.presence_of_element_located(toast_loc))
-        # time.sleep(1)
+        time.sleep(1)
+        e_success = self.driver.find_element_by_xpath("//*[@text='导入配置文件成功']")
+        self.driver.find_element_by_xpath("//*[@text='好的']").click()
+        time.sleep(1)
         self.driver.start_activity("com.example.app", ".MainActivity")
         time.sleep(2)
-        #正式发版前要修改，因为集成到别的apk上面是直接跳到列表页的
+        # 正式发版前要修改，因为集成到别的apk上面是直接跳到列表页的
         self.driver.find_element_by_id('com.example.app:id/btn_system_sms').click()
         self.driver.find_element_by_android_uiautomator("text(\"通知短信\")").click()
         ua_scroll = 'new UiScrollable(new UiSelector().className("android.support.v7.widget.RecyclerView")).scrollIntoView(new UiSelector().text("口碑网"))'
@@ -71,9 +74,14 @@ class MyTestCase(unittest.TestCase):
         time.sleep(1)
         self.driver.start_activity("com.cmic.testerapp", "com.cmic.testerapp.MainActivity")
         time.sleep(1)
+        # el = self.driver.find_element_by_id("com.cmic.testerapp:id/et_package_name")
+        # self.driver.set_value(el, "com.example.app.debug")
+        # time.sleep(1)
         self.driver.find_element_by_android_uiautomator("text(\"测试结果\")").click()
+
     def tearDown(self):
         pass
+
 
 if __name__ == '__main__':
     unittest.main()
